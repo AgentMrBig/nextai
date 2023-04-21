6b491e741c3ad767154c5dafbb4de42aeb73daa6
AgentMrBig <agentmrbig@gmail.com>
2023-04-21 00:36:37 -0400

    3 logos, styling started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,33 @@
+import stacks from "../data/stacks.json"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function Home() {
+
+  const renderStacks = () =>{
+    return Object.keys(stacks).map((stackKey) => {
+      const stack = stacks[stackKey];
+      return (
+        <Link 
+        key={stack.href}
+        href={stack.href}
+        className="w-20 h-20 relative border-2 border-solid m-2 rounded-xl"
+        >
+          <Image src={stack.logo} className="object-cover" fill alt=""/>
+        </Link>
+      );
+    })
+  }
+
+
   return (
-    <h1 className="text-3x1 font-bold underline">
-      Hello World!
+    <h1 className="h-full flex justify-center items-center flex-col">
+      <div className="">
+        What do you want to learn?
+      </div>
+      <div className="flex">
+        {renderStacks()}
+      </div>
     </h1>
   )
 }
